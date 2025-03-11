b87942b0f140a37179990ff853ebe09a43256c1d
mehdi-000 <mehdipopal@outlook.de>
2025-03-11 14:48:47 +0100

    fix:

--- app/components/gyrosensor.tsx
+++ app/components/gyrosensor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useDeviceOrientation } from "@/app/components/useDeviceOrientation";
+import { useDeviceOrientation } from "@/app/components/hooks/useDeviceOrientation";
 import { Toggle } from "./toogle";
 
 export const Gyro = () => {
