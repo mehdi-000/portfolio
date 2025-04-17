b4b54b2546ac5b23ce53396b0416d5161b7b43d4
Mehdi <mehdipopal@outlook.de>
2025-04-17 23:17:25 +0200

    feat: opt out tracking in `local` & `staging`

--- app/utils/ph-providers.tsx
+++ app/utils/ph-providers.tsx
@@ -7,21 +7,29 @@ import { usePostHog } from "posthog-js/react";
 import posthog from "posthog-js";
 import { PostHogProvider as PHProvider } from "posthog-js/react";
 
+const isPostHogEnabled =
+  typeof window !== "undefined" &&
+  process.env.NEXT_PUBLIC_ENABLE_POSTHOG === "true";
+
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      api_host:
-        process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
-      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
-      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
-    });
+    if (isPostHogEnabled) {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+        api_host:
+          process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+        person_profiles: "identified_only",
+        capture_pageview: false,
+      });
+    }
   }, []);
 
-  return (
+  return isPostHogEnabled ? (
     <PHProvider client={posthog}>
       <SuspendedPostHogPageView />
       {children}
     </PHProvider>
+  ) : (
+    children
   );
 }
 
